e39d62b899e440661a4eae6c90103b7ff0da1b3c
slava <simvesel@users.noreply.github.com>
2015-02-15 00:08:00 +0300

    Версия от 2011-10-02: добавлена передача параметров для утилиты, добавлено логирование. Сделан HELP...

--- src/ExtractSWF.cmd
+++ src/ExtractSWF.cmd
@@ -1,5 +1,16 @@
 @echo off
+set log=
+set cmdline=
+:next
+  if "%~1"=="" ( goto lbl1
+  ) else if "%~1"=="-log" ( set log=^>^>EXTRTSWF.log
+  ) else ( set cmdline=%cmdline% %1 )
+  shift
+goto next
+:lbl1
+:pause
+
 if not exist EXTRTSWF.exe echo Not Found EXTRTSWF.exe & goto END
-dir *.exe /A:-D /B /O:n /S | EXTRTSWF.exe -m -q
+dir *.exe /A:-D /B /O:n /S | EXTRTSWF.exe -m -q%cmdline% %log%
 :END
-pause>nul
+pause
